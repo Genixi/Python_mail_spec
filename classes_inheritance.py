23f0c66a268eb7bc89fdb6fa90c7226e6988627c
Admin <admin@iMac-Admin.local>
2019-08-22 15:40:22 +0300

    c1w3

--- classes_inheritance.py
+++ classes_inheritance.py
@@ -1,6 +1,24 @@
 import os
 import csv
 
+def get_car_list(csv_filename):
+	with open(csv_filename) as csv_fd:
+		reader = csv.reader(csv_fd, delimiter = ';')
+		next(reader)
+		car_list = []
+		for row in reader:
+			#print(row)
+			if len(row) != 0 and row[0] == "car":
+				#print("car choosen")
+				car_list.append(Car(row[1], row[3], row[-2], row[2]))
+			elif len(row) != 0 and row[0] == "truck":
+				#print("track choosen")
+				car_list.append(Truck(row[1], row[3], row[-2], row[-3]))
+			elif len(row) != 0 and row[0] == "spec_machine":
+				#print("spec_machine choosen")
+				car_list.append(SpecMachine(row[1], row[3], row[-2], row[-1]))
+		return car_list
+		
 class CarBase:
 	def __init__(self, brand, photo_file_name, carrying):
 		self.brand = brand
@@ -13,16 +31,16 @@ class CarBase:
 class Car(CarBase):
 	def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
 		super().__init__(brand, photo_file_name, carrying)
-		self.passenger_seats_count = passenger_seats_count
+		self.passenger_seats_count = int(passenger_seats_count)
 		self.car_type = "car"
 
 class Truck(CarBase):
 	def __init__(self, brand, photo_file_name, carrying, body_whl):
 		super().__init__(brand, photo_file_name, carrying)
 		self.body_whl = body_whl
-		self.body_length = get_body_length(body_whl)
-		self.body_width = get_body_width(body_whl)
-		self.body_height = get_body_height(body_whl)
+		self.body_length = self.get_body_length()
+		self.body_width = self.get_body_width()
+		self.body_height = self.get_body_height()
 		self.car_type = "truck"
 
 	def get_body_length(self):
@@ -63,19 +81,14 @@ class SpecMachine(CarBase):
 		self.extra = extra
 		self.car_type = "spec_machine"
 
-def get_car_list(csv_filename):
-	with open(csv_filename) as csv_fd:
-		reader = csv.reader(csv_fd, delimiter = ';')
-		next(reader)
-		car_list = []
-		for row in reader:
-			row_list = row.split(";")
-			if row_list[0] == "car":
-				car_list.append(Car(row_list[1], row_list[3], row_list[-1], row_list[2]))
-			elif row_list[0] == "truck":
-				car_list.append(Truck(row_list[1], row_list[3], row_list[-1], row_list[-2]))
-			elif row_list[0] == "spec_machine":
-				car_list.append(SpecMachine(ro_list[1], row_list[3], row_list[-2], row_list[-1]))
-
-	return car_list
-		
+'''
+cars = []
+#get_car_list("coursera_week3_cars.csv")
+cars = get_car_list("coursera_week3_cars.csv")
+for i in cars:
+	print("brand: {}, carrying: {}".format(i.brand, i.carrying))
+print(cars[1].body_length)
+print(cars[1].body_width)
+print(cars[1].body_height)
+print(cars[-1].extra)
+'''
